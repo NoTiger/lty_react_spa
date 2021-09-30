11cac31079debb2f6d177ab27e51a799b372b1bc
NoTiger <stevenyhtyeh@gmail.com>
2021-10-01 00:07:18 +0800

    Update say hi btn onClick

--- src/pages/Home/MyStory/MyStory.tsx
+++ src/pages/Home/MyStory/MyStory.tsx
@@ -3,14 +3,18 @@ import Button from "components/Button";
 import BlurTextBlock from "components/BlurTextBlock";
 import LogoHeader from "components/LogoHeader";
 import myStory from "images/myStory.jpg";
+import { useHistory } from "react-router-dom";
+import CONSTANT from "constant";
 
 export default function MyStory() {
+  const history = useHistory();
+
   const content = `In 2014, Graz took an unconventional path to travel around the world with only 2,000 HKD in her pocket.
    She was desperate to see the real world from the ground up--- the only vantage point from which it can be truly understood, socially, economically and politically. 
   This nomadic adventure has forever tuned the tone of her future.`;
 
   function onClick() {
-    return console.log("click");
+    history.push(CONSTANT.INTERNAL_LINKS.contact);
   }
 
   return (
